b3b7f541252ff0b072fd8b5b89717141ba6746c1
zuba2033 <saitman9999@gmail.com>
2022-07-02 17:14:35 +0300

    02.07.22 country

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import AppHeader from '../appHeader/AppHeader';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
+
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 // import AppBanner from '../appBanner/AppBanner';
 // import ComicsList from '../comicsList/ComicsList';
